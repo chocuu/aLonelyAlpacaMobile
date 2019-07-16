90a01b1b8ba083d4542d277052109bfefc834142
mlelien <emily.c.lien@gmail.com>
2019-07-15 20:05:34 -0400

    donate

---
+++ Assets/Scripts/Donate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Donate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DonateCompleted(UnityEngine.Purchasing.Product product)
+    {
+        Debug.Log("PURCHASE COMPLETE");
+    }
+
+    public void DonateFailed(UnityEngine.Purchasing.Product product, UnityEngine.Purchasing.PurchaseFailureReason failureReason)
+    {
+        Debug.Log("PURCHASE FAILED");
+    }
+}

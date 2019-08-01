450edc8b2e88645bab3463003c9a46f942c361db
mlelien <emily.c.lien@gmail.com>
2019-07-31 21:53:56 -0400

    new donations

--- Assets/Scripts/Donate.cs
+++ Assets/Scripts/Donate.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Donate : MonoBehaviour
 {
+    [SerializeField] GameObject thanksObj;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +20,16 @@ public class Donate : MonoBehaviour
     public void DonateCompleted(UnityEngine.Purchasing.Product product)
     {
         Debug.Log("PURCHASE COMPLETE");
+        thanksObj.SetActive(true);
     }
 
     public void DonateFailed(UnityEngine.Purchasing.Product product, UnityEngine.Purchasing.PurchaseFailureReason failureReason)
     {
         Debug.Log("PURCHASE FAILED");
+    }
+
+    public void ThanksClosed()
+    {
+        thanksObj.SetActive(false);
     }
 }
